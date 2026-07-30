57210d5498db687ac2230b5efce51d33cceb8a55
Vinay Kumar Endla <kumar.ve789@gmail.com>
2026-07-30 15:10:45 -0400

    Module 3 - Add operators and utility bill calculation

---
+++ 01-Python-Basics/03_operators.py
@@ -0,0 +1,16 @@
+customer_name = "John Smith"
+monthly_usage = 842
+solar_customer = True
+
+RATE_PER_KWH = 0.15345
+FIXED_CHARGE = 25
+
+energy_charge = monthly_usage * RATE_PER_KWH
+total_charge = energy_charge + FIXED_CHARGE
+
+print(f"{'*='*40}")
+print(f"Customer: {customer_name}")
+print(f"Monthly Usage    : {monthly_usage} kWh")
+print(f"Energy Charge    : ${energy_charge:.2f}")
+print(f"Fixed Charge     : ${FIXED_CHARGE:.2f}")
+print(f"Total Charge     : ${total_charge:.2f}")
